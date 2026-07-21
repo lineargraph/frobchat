cc349624e730ad70120d88de7ac526c7b0b1aab3
Linnea Gräf <nea@nea.moe>
2026-07-21 17:40:43 +0200

    auth token

--- json/matrix/src/main/java/moe/nea/frobchat/matrixapi/MatrixAuthentication.java
+++ json/matrix/src/main/java/moe/nea/frobchat/matrixapi/MatrixAuthentication.java
@@ -7,10 +7,25 @@ public interface MatrixAuthentication {
 
 	void applyTo(HttpRequest.Builder request);
 
+	default MatrixAuthentication withToken(String token) {
+		return token(baseUrl(), token);
+	}
+
 	record Anon(String baseUrl) implements MatrixAuthentication {
 		@Override
 		public void applyTo(HttpRequest.Builder request) {
 		}
+	}
+
+	record AuthToken(String baseUrl, String token) implements MatrixAuthentication {
+		@Override
+		public void applyTo(HttpRequest.Builder request) {
+			request.header("authorization", "Bearer " + token);
+		}
+	}
+
+	static MatrixAuthentication token(String baseUrl, String token) {
+		return new AuthToken(baseUrl, token);
 	}
 
 	static MatrixAuthentication anon(String baseUrl) {
